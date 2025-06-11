19f9b98151ec47c8fabca4f5ce8c2ffdd078656f
SahanThiwanka <sahanthiwanka1234@gmail.com>
2025-06-11 22:30:54 +0530

    error fixt 4

--- components/ui/link-preview.tsx
+++ components/ui/link-preview.tsx
@@ -9,6 +9,7 @@ import {
   useMotionValue,
   useSpring,
 } from "motion/react";
+import Image from "next/image";
 
 import { cn } from "@/lib/utils";
 
@@ -31,6 +32,8 @@ export const LinkPreview = ({
   className,
   width = 200,
   height = 125,
+  quality = 50,
+  layout = "fixed",
   isStatic = false,
   imageSrc = "",
 }: LinkPreviewProps) => {
@@ -76,12 +79,7 @@ export const LinkPreview = ({
     <>
       {isMounted ? (
         <div className="hidden">
-          <img
-            src={src}
-            width={width}
-            height={height}
-            alt="hidden image"
-          />
+          <Image src={src} width={width} height={height} alt="hidden image" />
         </div>
       ) : null}
 
@@ -131,7 +129,7 @@ export const LinkPreview = ({
                   className="block p-1 bg-white border-2 border-transparent shadow rounded-xl hover:border-neutral-200 dark:hover:border-neutral-800"
                   style={{ fontSize: 0 }}
                 >
-                  <img
+                  <Image
                     src={isStatic ? imageSrc : src}
                     width={width}
                     height={height}
