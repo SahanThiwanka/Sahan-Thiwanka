e98f05472ec7e93f7ca377f6ee8bc843aa4cfba3
SahanThiwanka <sahanthiwanka1234@gmail.com>
2025-06-14 19:26:32 +0530

    logo

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -238,12 +238,11 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <Image
-        src="https://assets.aceternity.com/logo-dark.png"
+        src="/images/logo.png"
         alt="logo"
-        width={30}
+        width={60}
         height={30}
       />
-      <span className="font-medium text-black dark:text-white">Startup</span>
     </a>
   );
 };
